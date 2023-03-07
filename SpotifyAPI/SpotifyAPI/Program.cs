5645ede6adf1c042b9d23cd8bd7185111d1170f5
ZaripovRamil <zaripoff.ramil@gmail.com>
2023-03-07 20:07:46 +0300

    Added DbContext

--- SpotifyAPI/SpotifyAPI/Program.cs
+++ SpotifyAPI/SpotifyAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SpotifyAPI.Models.AppDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext.AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OurDatabase")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
