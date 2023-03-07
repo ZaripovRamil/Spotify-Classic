9c0db8024617206df1c2a9dc542cf2dc5f74b1b2
ZaripovRamil <zaripoff.ramil@gmail.com>
2023-03-07 20:08:47 +0300

    quick fix

--- SpotifyAPI/SpotifyAPI/Program.cs
+++ SpotifyAPI/SpotifyAPI/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext.AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("OurDatabase")));
 var app = builder.Build();
 
