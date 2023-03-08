b45daa3e19a7e5bfc91eae0bee1f01ea61ea4ffb
ZaripovRamil <zaripoff.ramil@gmail.com>
2023-03-09 00:18:18 +0300

    Added new files

---
+++ SpotifyAPI/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace SpotifyAPI;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Track> Genres { get; set; }
+    public DbSet<Album> Albums { get; set; }
+    public DbSet<Track> Tracks { get; set; }
+    public DbSet<User>Users { get; set; }
+    public DbSet<Playlist>Playlists { get; set; }
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
