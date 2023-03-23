8e4df484ec7db8f725cea5d70e50da57b7be07ea
scilightener <nikita.l.boss@gmail.com>
2023-03-24 00:33:53 +0300

    minor changes to ui

--- front/src/authorization/AuthorizationPage.jsx
+++ front/src/authorization/AuthorizationPage.jsx
@@ -6,11 +6,18 @@ import wheel from "./media/playing_record_wheel.png";
 const AuthorizationPage = () => {
     return (
         <>
+            {/* global header */}
             <header></header>
             <main>
-                <img src={wheel} width={909} style={{ position: "absolute", left: "-454px" }} />
+                {/* maybe move to separate components?
+                * also, this is not even a bit adaptive... */}
+                <div className="circle" id="first" />
+                {/* play with these circles' top, left, width and height so that they won't affect overflow */}
+                <div className="circle" id="second" />
+                <img src={wheel} width={909} style={{ position: "absolute", left: "-454px", filter: "contrast(.9)" }} />
                 <AuthForm />
             </main>
+            {/* global footer */}
             <footer></footer>
         </>
     )
