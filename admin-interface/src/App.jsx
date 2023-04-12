669926962154cbd168b993155cd33d24766dda8c
scilightener <nikita.l.boss@gmail.com>
2023-04-13 01:25:25 +0300

    refactored admin table & added material design

--- admin-interface/src/App.jsx
+++ admin-interface/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tracks from './tracksPage/Tracks';
 import Albums from './albumsPage/Albums';
@@ -7,12 +7,29 @@ import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 },
+  ]);
+  const columns = [
+    {
+      name: "name",
+      label: "name",
+      type: "text",
+    },
+    {
+      name: "age",
+      label: "age",
+      type: "number",
+    }
+  ];
   return (
     <>
       <BrowserRouter>
         <Menu />
         <Routes>
-          <Route path="/tracks" element={<Tracks />} />
+          <Route path="/tracks" element={<Tracks data={data} onDataChange={setData} columns={columns} />} />
           <Route path="/albums" element={<Albums />} />
           <Route path="/authors" element={<Authors />} />
         </Routes>
--- admin-interface/src/tracksPage/Tracks.jsx
+++ admin-interface/src/tracksPage/Tracks.jsx
@@ -1,128 +1,192 @@
-import { Fragment } from "react";
-import React, { useState } from "react";
-import "./Tracks.css";
+import { Fragment, useState } from "react";
+import {
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+  IconButton,
+  Paper,
+  Button,
+  TextField,
+  makeStyles
+} from '@material-ui/core';
+import {
+  Edit as EditIcon,
+  Delete as DeleteIcon,
+  Save as SaveIcon,
+  Cancel as CancelIcon,
+  Add as AddIcon
+} from "@material-ui/icons";
 
-const Tracks = () => {
-  const columns = ["name", "age"];
-  const [data, setData] = useState([
-    { name: "Alice", age: 25 },
-    { name: "Bob", age: 30 },
-    { name: "Charlie", age: 35 },
-  ]);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '80%',
+    margin: '0 auto',
+    backgroundColor: theme.palette.background.paper,
+  },
+  table: {
+    minWidth: 650,
+    borderCollapse: "collapse",
+    "& td, & th": {
+      border: "1px solid #ddd",
+      padding: 8,
+      textAlign: "left"
+    }
+  },
+  addButton: {
+    background: "#4caf50",
+    color: "white"
+  },
+  editButton: {
+    background: "#2196f3",
+    color: "white"
+  },
+  deleteButton: {
+    background: "#f44336",
+    color: "white"
+  },
+  saveButton: {
+    background: "#4caf50",
+    color: "white"
+  },
+  cancelButton: {
+    background: "#ccc",
+    color: "white"
+  },
+  input: {
+    marginBottom: 8,
+    borderRadius: 4,
+    border: '1px solid #ddd'
+  },
+  formContainer: {
+    padding: '16px',
+    marginTop: '16px',
+    borderRadius: '4px',
+    backgroundColor: '#f9f9f9',
+    border: '1px solid #ddd'
+  }
+}));
 
+const Tracks = ({ data, onDataChange, columns }) => {
   const [editIndex, setEditIndex] = useState(-1);
-  const [newData, setNewData] = useState({ name: "", age: 0 });
+  const [newData, setNewData] = useState({});
 
   const handleSave = (index) => {
     const updatedData = [...data];
     updatedData[index] = { ...updatedData[index], ...newData };
-    setData(updatedData);
+    onDataChange(updatedData);
     setEditIndex(-1);
-    setNewData({ name: "", age: 0 });
+    setNewData({});
   };
 
   const handleCancel = () => {
+    // if the row was created by add track and then canceled immediately
+    if (!Object.values(data[editIndex]).some(Boolean)) {
+      handleDelete(editIndex);
+    }
     setEditIndex(-1);
-    setNewData({ name: "", age: 0 });
+    setNewData({});
   };
 
   const handleAdd = () => {
     const updatedData = [{ ...newData }, ...data];
-    setData(updatedData);
+    onDataChange(updatedData);
     setEditIndex(0);
-    setNewData({ name: "", age: 0 });
+    setNewData({});
   };
 
   const handleDelete = (index) => {
     const updatedData = [...data];
     updatedData.splice(index, 1);
-    setData(updatedData);
+    onDataChange(updatedData);
   };
 
+  const classes = useStyles();
+
   return (
-    <div className="table-container">
-      <button className="button button-add" onClick={handleAdd}>
+    <div className={classes.root}>
+      <Button
+        variant="contained"
+        className={classes.addButton}
+        onClick={handleAdd}
+      >
         Add Track
-      </button>
-      <table className="table">
-        <thead>
-          <tr>
-            {columns.map((column) => (
-              <th key={column}>{column}</th>
+      </Button>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            {columns.map((column, index) => (
+              <TableCell key={`column-${index}`}>{column.label}</TableCell>
             ))}
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {data.map((item, index) => (
-            <Fragment key={index}>
+            <Fragment key={`data-${index}`}>
               {editIndex === index ? (
-                <tr>
-                  <td>
-                    <input
-                      className="input"
-                      type="text"
-                      value={newData.name}
-                      onChange={(e) =>
-                        setNewData({ ...newData, name: e.target.value })
-                      }
-                    />
-                  </td>
-                  <td>
-                    <input
-                      className="input"
-                      type="text"
-                      value={newData.age}
-                      onChange={(e) =>
-                        setNewData({
-                          ...newData,
-                          age: parseInt(e.target.value),
-                        })
-                      }
-                    />
-                  </td>
-                  <td>
-                    <button
-                      className="button button-save"
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell key={column.name}>
+                      <TextField
+                        className={classes.input}
+                        type={column.type || "text"}
+                        value={newData[column.name] || ""}
+                        onChange={(e) =>
+                          setNewData({ ...newData, [column.name]: e.target.value })
+                        }
+                        {...(column.props || {})}
+                      />
+                    </TableCell>
+                  ))}
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      className={classes.saveButton}
                       onClick={() => handleSave(index)}
                     >
-                      Save
-                    </button>
-                    <button
-                      className="button button-cancel"
+                      <SaveIcon />
+                    </Button>
+                    <Button
+                      variant="contained"
+                      className={classes.cancelButton}
                       onClick={handleCancel}
                     >
-                      Cancel
-                    </button>
-                  </td>
-                </tr>
+                      <CancelIcon />
+                    </Button>
+                  </TableCell>
+                </TableRow>
               ) : (
-                <tr>
-                  <td>{item.name}</td>
-                  <td>{item.age}</td>
-                  <td>
-                    <button
-                      className="button button-edit"
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell key={column.name}>{item[column.name]}</TableCell>
+                  ))}
+                  <TableCell>
+                    <IconButton
+                      aria-label="edit"
+                      className={classes.editButton}
                       onClick={() => {
                         setEditIndex(index);
                         setNewData(item);
                       }}
                     >
-                      Edit
-                    </button>
-                    <button
-                      className="button button-delete"
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton
+                      aria-label="delete"
+                      className={classes.deleteButton}
                       onClick={() => handleDelete(index)}
                     >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
               )}
             </Fragment>
           ))}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
     </div>
   );
 };
