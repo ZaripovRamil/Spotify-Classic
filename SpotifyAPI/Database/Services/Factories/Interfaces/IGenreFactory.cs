d6deb6e1da3839c96f33b9589db3df602e9dc91e
ZaripovRamil <zaripoff.ramil@gmail.com>
2023-04-13 19:49:34 +0300

    Added new factory

---
+++ SpotifyAPI/Database/Services/Factories/Interfaces/IGenreFactory.cs
@@ -0,0 +1,10 @@
+﻿using Models.DTO.BackToFront.EntityCreationResult;
+using Models.DTO.FrontToBack.EntityCreationData;
+using Models.Entities;
+
+namespace Database.Services.Factories.Interfaces;
+
+public interface IGenreFactory
+{
+    public Task<(GenreCreationCode, Genre?)> Create(GenreCreationData data);
+}
