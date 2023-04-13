1ca2eaa06474e28e5677defd4ac102dfc1cffac9
ZaripovRamil <zaripoff.ramil@gmail.com>
2023-04-13 09:39:25 +0300

    fix

--- SpotifyAPI/Models/DTO/BackToFront/EntityCreationResult/RegistrationResult.cs
+++ SpotifyAPI/Models/DTO/BackToFront/EntityCreationResult/RegistrationResult.cs
@@ -7,7 +7,9 @@ public class RegistrationResult
     private static readonly Dictionary<RegistrationCode, string> Messages = new() {
         {RegistrationCode.Successful, "Success"},
         {RegistrationCode.EmailTaken, "Email is taken"},
-        {RegistrationCode.LoginTaken, "Login is taken"}
+        {RegistrationCode.LoginTaken, "Login is taken"},
+        {RegistrationCode.WeakPassword, "Weak password"},
+        {RegistrationCode.UnknownError, "Unknown error"}
     };
     public bool IsSuccessful { get; set; }
     public string? UserId { get; set; }
