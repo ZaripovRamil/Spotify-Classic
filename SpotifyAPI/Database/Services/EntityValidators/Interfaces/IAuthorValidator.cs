c92c66a001655321e9ab22e95b710e9667e91e76
scilightener <nikita.l.boss@gmail.com>
2023-04-22 14:28:47 +0300

    fixed merge conflicts

--- SpotifyAPI/Database/Services/EntityValidators/Interfaces/IAuthorValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Models.DTO.FrontToBack.EntityCreationData;
-using Models.DTO.InterServices.ValidationResult;
-
-namespace Database.Services.EntityValidators.Interfaces;
-
-public interface IAuthorValidator
-{
-    public Task<AuthorValidationResult> Validate(AuthorCreationData data);
-}
--- SpotifyAPI/Database/Services/EntityValidators/Interfaces/ITrackValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Models.DTO.FrontToBack.EntityCreationData;
-using Models.DTO.InterServices.ValidationResult;
-
-namespace Database.Services.EntityValidators.Interfaces;
-
-public interface ITrackValidator
-{
-    public Task<TrackValidationResult> Validate(TrackCreationData data);
-}
