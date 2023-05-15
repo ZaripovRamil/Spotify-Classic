8e243463dbffc4c43621a1a93ca462c64b673df2
RamilZaripov <zaripoff.ramil@gmail.com>
2023-05-15 15:24:27 +0300

    Playlist controller extended

---
+++ SpotifyAPI/Models/DTO/PlaylistTrackAdditionDataWithUser.cs
@@ -0,0 +1,19 @@
+﻿using Models.DTO.FrontToBack;
+
+namespace Models.DTO;
+
+public class PlaylistTrackAdditionDataWithUser : PlaylistTrackAdditionData
+{
+    public string UserName { get; set; }
+
+    public PlaylistTrackAdditionDataWithUser(PlaylistTrackAdditionData data, string userName)
+    {
+        TrackId = data.TrackId;
+        UserName = userName;
+        PlaylistId = data.PlaylistId;
+    }
+
+    public PlaylistTrackAdditionDataWithUser()
+    {
+    }
+}
--- SpotifyAPI/PlayerAPI/Controllers/PlaylistsController.cs
+++ SpotifyAPI/PlayerAPI/Controllers/PlaylistsController.cs
@@ -1,9 +1,14 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Models;
+using Models.DTO;
 using Models.DTO.BackToFront.Full;
 using Models.DTO.BackToFront.Light;
+using Models.DTO.FrontToBack;
 
 namespace PlayerAPI.Controllers;
 
@@ -25,7 +30,7 @@ public class PlaylistsController : Controller
     {
         var playlist = await _clientToDb.GetFromJsonAsync<PlaylistFull>($"get/id/{id}");
         if (playlist is null) return NotFound();
-        return new JsonResult(new PlaylistLight(playlist));
+        return new JsonResult(playlist);
     }
 
     [HttpGet("get")]
@@ -33,5 +38,17 @@ public class PlaylistsController : Controller
     {
         var playlists = await _clientToDb.GetFromJsonAsync<IEnumerable<PlaylistFull>>("get");
         return new JsonResult(playlists?.Select(playlist => new PlaylistLight(playlist)));
+    }
+
+    [HttpPost("addtrack")]
+    public async Task<IActionResult> AddTrack([FromBody] PlaylistTrackAdditionData data)
+    {
+        var json = JsonSerializer.Serialize(new PlaylistTrackAdditionDataWithUser(data, User.Identity.Name));
+        var resp = await _clientToDb.PostAsync("AddTrack", new StringContent(json, Encoding.UTF8, "application/json"));
+        if (resp.IsSuccessStatusCode)
+            return Ok();
+        if (resp.StatusCode == HttpStatusCode.Forbidden)
+            return Forbid();
+        return BadRequest();
     }
 }
