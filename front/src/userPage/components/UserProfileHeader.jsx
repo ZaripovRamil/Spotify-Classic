8bd84570449ef5b72a0a9c46bcb3cfbf9bebb4ca
kamilla11 <gafarova.kamilla@list.ru>
2023-05-29 14:56:10 +0300

    little fix

--- front/src/userPage/components/UserProfileHeader.jsx
+++ front/src/userPage/components/UserProfileHeader.jsx
@@ -13,18 +13,22 @@ export const UserProfileHeader = ({
         <div className="userAvatar"></div>
         <div className="header-info">
           <NavLink to="/user">
-            <div className="userName">{userName}</div>
-            {subscribtionName !== "" && (
-              <img
-                src={crown}
-                alt={subscribtionName}
-                style={{ width: "40px" }}
-              />
-            )}
+            <div style={{ display: "flex" }}>
+              <div className="userName">{userName}</div>
+              {subscribtionName !== "" && (
+                <img
+                  src={crown}
+                  alt={subscribtionName}
+                  style={{ width: "40px" }}
+                />
+              )}
+            </div>
           </NavLink>
           {component}
           {/* only for authorized */}
-          <NavLink to="/user/statistic">Export data</NavLink>
+          {subscribtionName !== "" && (
+            <NavLink to="/user/statistic">Export data</NavLink>
+          )}
           <NavLink to="/user/subscription">Subscription</NavLink>
         </div>
       </div>
