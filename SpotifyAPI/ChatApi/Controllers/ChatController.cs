4bd96a8902aeef45cafab1dd0856d56c0f56dca5
Kuchina Olga <olmenua2003@gmail.com>
2023-09-25 00:46:46 +0300

    remove chat warnings

--- SpotifyAPI/ChatApi/Controllers/ChatController.cs
+++ SpotifyAPI/ChatApi/Controllers/ChatController.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
 using Models.DTO.FrontToBack.Chat;
 
 namespace ChatApi.Controllers;
@@ -12,17 +11,15 @@ namespace ChatApi.Controllers;
 [Authorize]
 public class ChatController : Controller
 {
-    private readonly IHubContext<ChatHub> _hubCnt;
-
     [HttpGet("[action]")]
-    public async Task<List<ChatMessage>> History()
+    public List<ChatMessage> History()
     {
-        var username = User.Identity.Name;
-        if (!ChatService.MessageHistory.ContainsKey(username))
+        var username = User.Identity!.Name;
+        if (!ChatService.MessageHistory.ContainsKey(username!))
         {
-            ChatService.MessageHistory[username] = new List<ChatMessage>();
+            ChatService.MessageHistory[username!] = new List<ChatMessage>();
         }
-        var history = ChatService.MessageHistory[username].Select(m => new ChatMessage()
+        var history = ChatService.MessageHistory[username!].Select(m => new ChatMessage()
         {
             Message = m.Message,
             User = m.User,
@@ -33,7 +30,7 @@ public class ChatController : Controller
     
     [Authorize(Roles = "Admin")]
     [HttpGet("[action]/{groupname}")]
-    public async Task<List<ChatMessage>> History(string groupname)
+    public List<ChatMessage> History(string groupname)
     {
         if (!ChatService.MessageHistory.ContainsKey(groupname))
         {
