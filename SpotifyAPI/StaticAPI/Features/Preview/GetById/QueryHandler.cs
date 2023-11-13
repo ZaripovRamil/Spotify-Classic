924dd81895c7157951ad0fd1ddb55aef0f130d8c
Kuchina Olga <olmenua2003@gmail.com>
2023-11-13 14:45:18 +0300

    fix sonar warning

--- SpotifyAPI/StaticAPI/Features/Preview/GetById/QueryHandler.cs
+++ SpotifyAPI/StaticAPI/Features/Preview/GetById/QueryHandler.cs
@@ -12,11 +12,11 @@ public class QueryHandler : IQueryHandler<Query,Stream>
         _fileProvider = fp;
     }
 
-    public async Task<Result<Stream>> Handle(Query request, CancellationToken cancellationToken)
+    public Task<Result<Stream>> Handle(Query request, CancellationToken cancellationToken)
     {
         var preview = _fileProvider.GetFileAsStream("Previews", $"{request.Id}.jpg");
         return preview is null ?
-            new Result<Stream>("Preview not found") :
-            new Result<Stream>(preview);
+            Task.FromResult(new Result<Stream>("Preview not found") ):
+            Task.FromResult(new Result<Stream>(preview));
     }
 }
