c2e305bbb24ac4a34c3f494b687094fd48686821
Ramil <zaripoff.ramil@gmail.com>
2023-12-18 00:52:27 +0300

    calming codecov v2

--- SpotifyAPI/External.Redis/RedisCache.cs
+++ SpotifyAPI/External.Redis/RedisCache.cs
@@ -32,6 +32,7 @@ public class RedisCache : IRedisCache
 
     public async Task Set<T>(T value) where T : Metadata
     {
+        if (value.FileId is null) throw new InvalidDataException();
         await _cache.SetStringAsync(value.FileId, JsonSerializer.Serialize(value));
     }
 
