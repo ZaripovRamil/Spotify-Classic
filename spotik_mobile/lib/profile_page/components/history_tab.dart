d628d898b6d6f24a7cdbaa55ebf75e46daa656a7
scilightener <nikita.l.boss@gmail.com>
2024-05-13 13:46:40 +0300

    tappable tracks in history

--- spotik_mobile/lib/profile_page/components/history_tab.dart
+++ spotik_mobile/lib/profile_page/components/history_tab.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:spotik_mobile/models/entity/track_light/track.dart';
+import 'package:spotik_mobile/models/player_provider.dart';
 import 'package:spotik_mobile/profile_page/bloc/history/history_bloc.dart';
 import 'package:spotik_mobile/utils/constants/resources.dart';
-import 'package:spotik_mobile/utils/ui_constants.dart';
 
 class HistoryTab extends StatelessWidget {
   const HistoryTab({super.key});
@@ -13,23 +13,7 @@ class HistoryTab extends StatelessWidget {
     final state = context.watch<HistoryBloc>().state;
     return Column(
       children: [
-        Align(
-          alignment: Alignment.centerRight,
-          child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: MaterialButton(
-                  color: CustomColors.backgroundButtonGrey,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(13.0),
-                  ),
-                  onPressed: () => {
-                        context
-                            .read<HistoryBloc>()
-                            .add(const HistoryEvent.clear())
-                      },
-                  child: const Text('Clear',
-                      style: TextStyle(fontSize: TextSize.smallTextSize)))),
-        ),
+        const SizedBox(height: 10),
         Expanded(
             child: state.when(
                 initial: () => const Text("History is empty"),
@@ -57,19 +41,27 @@ class HistoryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: tracks.length,
-      itemBuilder: (context, index) {
-        final item = tracks[index];
-        return ListTile(
-          leading: Image.network(
-            Endpoints.getPreviewUrl(item.album.previewId),
-            fit: BoxFit.cover,
-          ),
-          title: Text(item.name),
-          subtitle: Text(item.album.name),
-        );
-      },
-    );
+    return Consumer<PlayerProvider>(builder: (context, value, child) {
+      var playerProvider = Provider.of<PlayerProvider>(context, listen: false);
+      return ListView.builder(
+        itemCount: tracks.length,
+        itemBuilder: (context, index) {
+          final item = tracks[index];
+          return ListTile(
+            onTap: () {
+              playerProvider.tracklist = tracks;
+              value.currentTrackListId = tracks[index].album.id;
+              playerProvider.currentTrackIndex = index;
+            },
+            leading: Image.network(
+              Endpoints.getPreviewUrl(item.album.previewId),
+              fit: BoxFit.cover,
+            ),
+            title: Text(item.name),
+            subtitle: Text(item.album.name),
+          );
+        },
+      );
+    });
   }
 }
